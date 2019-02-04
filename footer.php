6cbab35725ee5b10d432dd0358605eb7546ede80
Fabio Tutiashi <fabiotutiashi@outlook.com>
2019-02-03 22:04:05 -0200

    Criando header e footer separadamente

---
+++ footer.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+<title>Cart</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Little Closet template">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="styles/bootstrap-4.1.2/bootstrap.min.css">
+<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="styles/cart.css">
+<link rel="stylesheet" type="text/css" href="styles/cart_responsive.css">
+</head>
+<body>
+
+
+
+<!-- Footer -->
+
+		<footer class="footer">
+			<div class="footer_content">
+				<div class="container">
+					<div class="row">
+						
+						<!-- About -->
+						<div class="col-lg-4 footer_col">
+							<div class="footer_about">
+								<div class="footer_logo">
+									<a href="#">
+										<div class="d-flex flex-row align-items-center justify-content-start">
+											<div class="footer_logo_icon"><img src="images/logo_2.png" alt=""></div>
+											<div>Produtos Sustentáveis</div>
+										</div>
+									</a>		
+								</div>
+								<div class="footer_about_text">
+									<p>Semear ideias ecológicas e plantar sustentabilidade é ter a garantia de colhermos um futuro fértil e consciente</p>
+								</div>
+							</div>
+						</div>
+
+						<!-- Footer Links -->
+						<div class="col-lg-4 footer_col">
+							<div class="footer_menu">
+								<div class="footer_title">Suporte</div>
+								<ul class="footer_list">
+									<li>
+										<a href="#"><div>Serviço ao Cliente<div class="footer_tag_1">online agora</div></div></a>
+									</li>
+									<li>
+										<a href="#"><div>Politica de Devolução</div></a>
+									</li>
+									<li>
+										<a href="#"><div>Guia<div class="footer_tag_2">recomendado</div></div></a>
+									</li>
+									<li>
+										<a href="#"><div>Termos e Condições</div></a>
+									</li>
+									<li>
+										<a href="#"><div>Contato</div></a>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+						<!-- Footer Contact -->
+						<div class="col-lg-4 footer_col">
+							<div class="footer_contact">
+								<div class="footer_title">Entre em Contato</div>
+								<div class="newsletter">
+									<form action="#" id="newsletter_form" class="newsletter_form">
+										<input type="email" class="newsletter_input" placeholder="Digite seu e-mail" required="required">
+										<button class="newsletter_button">+</button>
+									</form>
+								</div>
+								<div class="footer_social">
+									<div class="footer_title">Social</div>
+									<ul class="footer_social_list d-flex flex-row align-items-start justify-content-start">
+										<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+										<li><a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+										<li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+										<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="footer_bar">
+				<div class="container">
+					<div class="row">
+						<div class="col">
+							<div class="footer_bar_content d-flex flex-md-row flex-column align-items-center justify-content-start">
+								<div class="copyright order-md-1 order-2"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script>Todos os direitos reservados | Este modelo é feito com <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
+								<nav class="footer_nav ml-md-auto order-md-2 order-1">
+									<ul class="d-flex flex-row align-items-center justify-content-start">
+										<li><a href="category.html">Mulher</a></li>
+										<li><a href="category.html">Homem</a></li>
+										<li><a href="category.html">Kids</a></li>
+										<li><a href="category.html">Home</a></li>
+										<li><a href="#">Contato</a></li>
+									</ul>
+								</nav>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</div>
+		
+</div>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="styles/bootstrap-4.1.2/popper.js"></script>
+<script src="styles/bootstrap-4.1.2/bootstrap.min.js"></script>
+<script src="plugins/greensock/TweenMax.min.js"></script>
+<script src="plugins/greensock/TimelineMax.min.js"></script>
+<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="plugins/greensock/animation.gsap.min.js"></script>
+<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="plugins/parallax-js-master/parallax.min.js"></script>
+<script src="js/cart.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+<title>Cart</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Little Closet template">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="styles/bootstrap-4.1.2/bootstrap.min.css">
+<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="styles/cart.css">
+<link rel="stylesheet" type="text/css" href="styles/cart_responsive.css">
+</head>
+<body>
+
+<!-- Menu -->
+
+<div class="menu">
+
+	<!-- Search -->
+	<div class="menu_search">
+		<form action="#" id="menu_search_form" class="menu_search_form">
+			<input type="text" class="search_input" placeholder="Search Item" required="required">
+			<button class="menu_search_button"><img src="images/search.png" alt=""></button>
+		</form>
+	</div>
+	<!-- Navigation -->
+	<div class="menu_nav">
+		<ul>
+			<li><a href="#">Mulher</a></li>
+			<li><a href="#">Homem</a></li>
+			<li><a href="#">Kids</a></li>
+			<li><a href="#">Home </a></li>
+			<li><a href="#">Contato</a></li>
+		</ul>
+	</div>
+	<!-- Contact Info -->
+	<div class="menu_contact">
+		<div class="menu_phone d-flex flex-row align-items-center justify-content-start">
+			<div><div><img src="images/phone.svg" alt="https://www.flaticon.com/authors/freepik"></div></div>
+			<div>11 5555-5555</div>
+		</div>
+		<div class="menu_social">
+			<ul class="menu_social_list d-flex flex-row align-items-start justify-content-start">
+				<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+				<li><a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+				<li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+				<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<div class="super_container">
+
+	<!-- Header -->
+
+	<header class="header">
+		<div class="header_overlay"></div>
+		<div class="header_content d-flex flex-row align-items-center justify-content-start">
+			<div class="logo">
+				<a href="#">
+					<div class="d-flex flex-row align-items-center justify-content-start">
+						<div><img src="images/logo_1.png" alt=""></div>
+						<div>Produtos Sustentáveis</div>
+					</div>
+				</a>	
+			</div>
+			<div class="hamburger"><i class="fa fa-bars" aria-hidden="true"></i></div>
+			<nav class="main_nav">
+				<ul class="d-flex flex-row align-items-start justify-content-start">
+					<li><a href="#">Mulher</a></li>
+					<li><a href="#">Homem</a></li>
+					<li><a href="#">Kids</a></li>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Contato</a></li>
+				</ul>
+			</nav>
+			<div class="header_right d-flex flex-row align-items-center justify-content-start ml-auto">
+				<!-- Search -->
+				<div class="header_search">
+					<form action="#" id="header_search_form">
+						<input type="text" class="search_input" placeholder="Procurar Item" required="required">
+						<button class="header_search_button"><img src="images/search.png" alt=""></button>
+					</form>
+				</div>
+				<!-- User -->
+				<div class="user"><a href="#"><div><img src="images/user.svg" alt="https://www.flaticon.com/authors/freepik"><div>1</div></div></a></div>
+				<!-- Cart -->
+				<div class="cart"><a href="cart.html"><div><img src="images/cart.svg" alt="https://www.flaticon.com/authors/freepik"></div></a></div>
+				<!-- Phone -->
+				<div class="header_phone d-flex flex-row align-items-center justify-content-start">
+					<div><div><img src="images/phone.svg" alt="https://www.flaticon.com/authors/freepik"></div></div>
+					<div>11 5555-5555</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+
+
+	
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="styles/bootstrap-4.1.2/popper.js"></script>
+<script src="styles/bootstrap-4.1.2/bootstrap.min.js"></script>
+<script src="plugins/greensock/TweenMax.min.js"></script>
+<script src="plugins/greensock/TimelineMax.min.js"></script>
+<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="plugins/greensock/animation.gsap.min.js"></script>
+<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="plugins/parallax-js-master/parallax.min.js"></script>
+<script src="js/cart.js"></script>
+</body>
+</html>
