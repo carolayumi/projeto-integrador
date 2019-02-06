7fbc8e6d8c8dd1f740f81020946f62a7de91b8d3
Fabio Tutiashi <fabiotutiashi@outlook.com>
2019-02-06 17:07:52 -0200

    Incluindo header e footer nos arquivos php

--- header.php
+++ header.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt">
-<head>
-<title>Cart</title>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="Little Closet template">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="styles/bootstrap-4.1.2/bootstrap.min.css">
-<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="styles/cart.css">
-<link rel="stylesheet" type="text/css" href="styles/cart_responsive.css">
-</head>
-<body>
+
 
 <!-- Menu -->
 
